ea1ebbd53e8f65abb60eb4dbb919cd5d46cc583c
ergemp <ergemp@gmail.com>
2022-05-18 15:14:17 +0300

    .

--- treasure_hunter/run.py
+++ treasure_hunter/run.py
@@ -2,8 +2,32 @@ ll = [[3,0,0,1,2],
       [0,1,4,0,0],
       [5,0,0,3,3]]
 
-for elem1 in ll:
-    print(elem1)
-    if len(elem1) > 0:
+
+def treasure_finder(gll):
+    map_final = []
+    row_final = []
+    for elem1 in gll:
+
+        if len(elem1) == 0:
+            return 0
+
+        row_total = 0
         for elem2 in elem1:
-            print(elem2)
+            if elem2 > 0:
+                row_total = row_total + elem2
+            elif elem2 == 0:
+                row_final.append(row_total)
+                row_total = 0
+            elif elem2 < 0:
+                return -1
+
+            row_final.append(row_total)
+
+        map_final.append(max(row_final))
+    return max(map_final)
+
+
+retval = treasure_finder(ll)
+print (retval)
+
+
